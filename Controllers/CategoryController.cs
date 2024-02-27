1f9ff5fd3ebcc4175a16d9cba3da817642e674e1
Ezelhan Dikmenlioğlu <20060394@stu.omu.edu.tr>
2024-02-27 23:28:32 +0300

    completed

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TasteFoodIt.Context;
+using TasteFoodIt.Entities;
 
 namespace TasteFoodIt.Controllers
 {
@@ -14,6 +15,40 @@ namespace TasteFoodIt.Controllers
         {
             var values = context.Categories.ToList();
             return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult CreateCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateCategory(Category category)
+        {
+            context.Categories.Add(category);
+            context.SaveChanges();
+            return RedirectToAction("CategoryList");
+        }
+        public ActionResult DeleteCategory(int id)
+        {
+            var values = context.Categories.Find(id);
+            context.Categories.Remove(values);
+            context.SaveChanges();
+            return RedirectToAction("CategoryList");
+        }
+        [HttpGet]
+        public ActionResult UpdateCategory(int id)
+        {
+            var value = context.Categories.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult UpdateCategory(Category category)
+        {
+            var value = context.Categories.Find(category.CategoryId);
+            value.CategoryName = category.CategoryName;
+            context.SaveChanges();
+            return RedirectToAction("CategoryList");
         }
     }
 }

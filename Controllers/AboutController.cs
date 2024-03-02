636373616af6683b4d262da3890757ce4b1cfdad
Ezelhan Dikmenlioğlu <20060394@stu.omu.edu.tr>
2024-03-02 21:52:36 +0300

    completed

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TasteFoodIt.Context;
+using TasteFoodIt.Entities;
+
+namespace TasteFoodIt.Controllers
+{
+    public class AboutController : Controller
+    {
+        TasteContext context = new TasteContext();
+        [HttpGet]
+        public ActionResult AboutList()
+        {
+            var values = context.Abouts.ToList();
+            return View(values);
+        }
+
+    }
+}

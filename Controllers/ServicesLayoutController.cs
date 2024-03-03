b8d790fcaa0152144e39baa5540a371f59a07cbe
Ezelhan Dikmenlioğlu <20060394@stu.omu.edu.tr>
2024-03-03 22:45:54 +0300

    completed

---
+++ Controllers/ServicesLayoutController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TasteFoodIt.Controllers
+{
+    public class ServicesLayoutController : Controller
+    {
+        // GET: Services
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult PartialServicesDetail()
+        {
+            ViewBag.Page = TempData["Page"];
+            return PartialView();
+        }
+    }
+}

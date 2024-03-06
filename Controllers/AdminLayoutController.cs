eb08dd959b9e097e904ae2cbc10fdb06d0e1323f
Ezelhan Dikmenlioğlu <20060394@stu.omu.edu.tr>
2024-03-06 08:43:38 +0300

    completed

--- Controllers/AdminLayoutController.cs
+++ Controllers/AdminLayoutController.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TasteFoodIt.Context;
 
 namespace TasteFoodIt.Controllers
 {
     public class AdminLayoutController : Controller
     {
-        // GET: AdminLayout
+        TasteContext context = new TasteContext();  
         public ActionResult Index()
         {
             return View();
@@ -23,7 +24,8 @@ namespace TasteFoodIt.Controllers
         }
         public PartialViewResult PartialNavbar()
         {
-            return PartialView();
+            var values = context.Notifications.Where(x =>x.IsRead == false).ToList();
+            return PartialView(values);
         }
         public PartialViewResult PartialFooter()
         {
@@ -33,6 +35,13 @@ namespace TasteFoodIt.Controllers
         public PartialViewResult PartialScript()
         {
             return PartialView();
+        }
+        public ActionResult NotificationStatusChangeToTrue(int id)
+        {
+            var values = context.Notifications.Find(id);
+            values.IsRead = true;
+            context.SaveChanges();
+            return RedirectToAction("CategoryList", "Category");
         }
     }
 }

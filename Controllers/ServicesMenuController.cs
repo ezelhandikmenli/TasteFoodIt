b23258d350ab310d205b923bf1b3b585052d9c7a
Ezelhan Dikmenlioğlu <20060394@stu.omu.edu.tr>
2024-03-11 00:30:01 +0300

    completed

---
+++ Controllers/ServicesMenuController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TasteFoodIt.Context;
+using TasteFoodIt.Entities;
+namespace TasteFoodIt.Controllers
+{
+    public class ServicesMenuController : Controller
+    {
+        TasteContext context = new TasteContext();
+        public ActionResult MenuList()
+        {
+            var value = context.Products.ToList();
+            return View(value);
+        }
+    }
+}

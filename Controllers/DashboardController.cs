46f939b3802864a5d3c9c0c7ee732724bdd968fb
Ezelhan Dikmenlioğlu <20060394@stu.omu.edu.tr>
2024-04-13 22:41:32 +0300

    ..

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using TasteFoodIt.Context;
 using TasteFoodIt.Entities;
 
+
 namespace TasteFoodIt.Controllers
 {
     public class DashboardController : Controller
@@ -19,5 +20,32 @@ namespace TasteFoodIt.Controllers
             ViewBag.v4 = context.Reservations.Where(x => x.ReservationStatus == "Aktif").Count();
             return View();
         }
+       public PartialViewResult js()
+        {
+            return PartialView();
+        }
+        public PartialViewResult Chart()
+        {
+            var value = context.Products.ToList();
+            return PartialView(value);
+        }
+        public PartialViewResult ProgressBar() {
+            ViewBag.mesaj = context.Contacts.Count();
+            ViewBag.musteri = context.Testimonials.Count();
+            ViewBag.social = context.SocialMedias.Count();
+            ViewBag.productprice = Convert.ToInt32(context.Products.Max(x => x.Price));
+            return PartialView();
+        }
+        public PartialViewResult Rezervasyon()
+        {
+            var value = context.Reservations.Take(5).ToList();
+            return PartialView(value);
+
+        }
+        public PartialViewResult Mesajlar() { 
+          var value = context.Contacts.Take(5).ToList();
+            return PartialView(value);  
+        }
+
     }
 }

d3d14d2851e7a9bbe15907562b81a84866e37402
Ezelhan Dikmenlioğlu <20060394@stu.omu.edu.tr>
2024-04-16 10:53:22 +0300

    ..

--- Entities/Admin.cs
+++ Entities/Admin.cs
@@ -10,5 +10,11 @@ namespace TasteFoodIt.Entities
         public int AdminId { get; set; }
         public string  UserName { get; set; }
         public string Password { get; set; } 
+        public string Name { get; set; }    
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string ImageUrl { get; set; }
+        public string PhoneNumber { get; set; }
+
     }
 }
